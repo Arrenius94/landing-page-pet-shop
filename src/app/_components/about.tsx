02ca831b82d1d86a5e62d2f739e126d555d2e7e1
Arrenius94 <arrenius94@gmail.com>
2025-10-15 23:32:56 -0300

    first commit

---
+++ src/app/_components/about.tsx
@@ -0,0 +1,95 @@
+import Image from "next/image";
+import aboutImg1 from "../../../public/about-1.png";
+import aboutImg2 from "../../../public/about-2.png";
+import { Check, MapPin } from "lucide-react";
+import { WhatsappLogoIcon } from "@phosphor-icons/react/dist/ssr";
+
+export function About() {
+  return (
+    <section className="bg-[#FDF6EC] py-16">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+          <div
+            className="relative"
+            data-aos="fade-up-right"
+            data-aos-delay="500"
+            data-aos-duration="1200"
+          >
+            <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
+              <Image
+                src={aboutImg1}
+                alt="Foto-Cachorro"
+                fill
+                quality={100}
+                className="object-cover hover:scale-110 duration-300"
+                priority
+              />
+            </div>
+
+            <div className="absolute w-40 h-40 right-4 -bottom-8 rounded-lg border-4 overflow-x-hidden border-white">
+              <Image
+                src={aboutImg2}
+                alt="Foto-Gato"
+                fill
+                quality={100}
+                priority
+              />
+            </div>
+          </div>
+
+          <div
+            className="space-y-6 mt-10"
+            data-aos="fade-up-left"
+            ata-aos-duration="4000"
+            data-aos-delay="500"
+          >
+            <h2 className="text-4xl font-bold">SOBRE</h2>
+            <p>
+              Até que alguém ame um animal, uma parte da sua alma permanece
+              adormecida. Acreditamos nisso e acreditamos no fácil acesso a
+              coisas que fazem bem à mente, ao corpo e ao espírito. Com uma
+              oferta inteligente, um suporte excelente e um checkout seguro,
+              você está em boas mãos.
+            </p>
+
+            <ul className="space-y-4">
+              <li className="flex items-center gap-2">
+                <Check className="text-red-500" />
+                Aberto Desde 2006
+              </li>
+
+              <li className="flex items-center gap-2">
+                <Check className="text-red-500" />
+                Equipe com mais de 10 veterinários
+              </li>
+
+              <li className="flex items-center gap-2">
+                <Check className="text-red-500" />
+                Qualidade é nossa prioridade
+              </li>
+            </ul>
+
+            <div className="flex gap-2">
+              <a
+                target="blank"
+                href={`https://wa.me/3499885855?text=Olá vim pelo site e gostaria de mais informações`}
+                className="bg-[#E84C3D] text-white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md hover:active:opacity-80"
+              >
+                <WhatsappLogoIcon className="w-5 h-5 text-white" />
+                Contato via WhatsApp
+              </a>
+
+              <a
+                href="#"
+                className=" flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
+              >
+                <MapPin className="w-5 h-5 text-black" />
+                Endereço da loja
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,110 @@
+import golden from "../../../public/golden.png";
+import royal from "../../../public/royal.png";
+import primier from "../../../public/primier.png";
+import whiskas from "../../../public/whiskas.png";
+import natural from "../../../public/natural.png";
+import Image from "next/image";
+import {
+  FacebookLogo,
+  FacebookLogoIcon,
+  InstagramLogo,
+  InstagramLogoIcon,
+  YoutubeLogo,
+  YoutubeLogoIcon,
+} from "@phosphor-icons/react/dist/ssr";
+
+const brands = [
+  { name: "Royal Canin", logo: royal },
+  { name: "Golden", logo: golden },
+  { name: "Primier", logo: primier },
+  { name: "Formula Natural", logo: natural },
+  { name: "Whiskas", logo: whiskas },
+  { name: "Golden", logo: golden },
+];
+
+export function Footer() {
+  return (
+    <section className="bg-[#E84c3d] py-16 text-white">
+      <div className="container mx-auto px-4">
+        <div className="border-b border-white/20 pb-8">
+          <h4
+            className="text-3xl font-semibold mb-8 text-center"
+            data-aos="zoom-in"
+            data-aos-delay="250"
+          >
+            Marcas que trabalhamos
+          </h4>
+
+          <div
+            className="grid grid-cols-2 lg:grid-cols-6 gap-8"
+            data-aos="zoom-in"
+            data-aos-delay="500"
+          >
+            {brands.map((item, index) => (
+              <div
+                key={index}
+                className="bg-white p-4 rounded-lg flex items-center justify-center"
+              >
+                <Image
+                  src={item.logo}
+                  alt={item.name}
+                  width={100}
+                  height={50}
+                  quality={100}
+                  style={{
+                    width: "auto",
+                    height: "auto",
+                  }}
+                  className="object-contain"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <footer
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12 mt-5"
+          data-aos="fade-up"
+          data-aos-duration="2000"
+        >
+          <div>
+            <h3 className="text-2xl font-semibold mb-2">Pet Shop Dev</h3>
+            <p className="mb-4">
+              Cuidando do seu melhor amigo com amor e dedicação.
+            </p>
+            <a
+              href="#"
+              className="bg-green-500 hover:active:opacity-80 px-4 py-2 rounded-md"
+            >
+              Contato via WhatsApp
+            </a>
+          </div>
+
+          <div>
+            <h3 className="text-2xl font-semibold mb-2">Contatos</h3>
+            <p>Email: teste@teste.com</p>
+            <p>Telefone: (XX) 1231831238</p>
+            <p>Rua X, centro, Campo Grande | MS</p>
+          </div>
+
+          <div>
+            <h3 className="text-2xl font-semibold mb-2 hover:active:opacity-80">
+              Redes sociais
+            </h3>
+            <div className="flex gap-4">
+              <a href="#" target="_blank" className="hover:active:opacity-80">
+                <FacebookLogoIcon className="w-8 h-8" />
+              </a>
+              <a href="#" target="_blank" className="hover:active:opacity-80">
+                <InstagramLogoIcon className="w-8 h-8" />
+              </a>
+              <a href="#" target="_blank" className="hover:active:opacity-80">
+                <YoutubeLogoIcon className="w-8 h-8" />
+              </a>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </section>
+  );
+}
